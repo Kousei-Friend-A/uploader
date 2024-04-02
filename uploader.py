854fd441668cde5cacd54c4e6e6e8716c8523800
Friend A <101492420+Kousei-Friend-A@users.noreply.github.com>
2024-04-02 12:00:36 +0530

    Update uploader.py

--- uploader.py
+++ uploader.py
@@ -87,7 +87,7 @@ async def start_uploader(client: Client, message: Message, url: str, proc: Messa
 
     t3 = time.time()
     await client.send_document(
-        chat_id="animeuploadteest1",
+        chat_id="-1002094965664",
         document="./files/" + filename,
         thumb="thumb.jpeg",
         caption=caption,
